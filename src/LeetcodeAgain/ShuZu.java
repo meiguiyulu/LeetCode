db7f2e71d67e9323ddbf0971a3f09486bc5eee0e
火之晨曦 <38106024+Excilbur@users.noreply.github.com>
2022-02-08 21:19:29 +0800

    leetcode again

---
+++ src/LeetcodeAgain/ShuZu.java
@@ -0,0 +1,28 @@
+package LeetcodeAgain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * LeetCode中数组类型的题目
+* */
+public class ShuZu {
+    /**
+     * 1. 两数之和
+     * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现
+     * 你可以按任意顺序返回答案。
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
