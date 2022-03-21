ae6d2ae14027d218c75af8f82150c3dd58792a3f
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-03-21 10:27:38 +0800

    17. 电话号码的字母组合

--- src/LeetcodeAgain/ZiFuChuan.java
+++ src/LeetcodeAgain/ZiFuChuan.java
@@ -124,8 +124,46 @@ public class ZiFuChuan {
         return ans;
     }
 
+    /**
+     * 17. 电话号码的字母组合
+     *
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinations(String digits) {
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        List<String> ans = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        if (digits.length() == 0) {
+            return ans;
+        }
+        dfsForLetter(digits, map, ans, builder, 0);
+        return ans;
+    }
+
+    private void dfsForLetter(String digits, Map<Character, String> map, List<String> ans, StringBuilder builder, int index) {
+        if (builder.length() == digits.length()) {
+            ans.add(builder.toString());
+            return;
+        }
+        String s1 = map.get(digits.charAt(index));
+        for (int i=0;i<s1.length();i++){
+            builder.append(s1.charAt(i));
+            dfsForLetter(digits, map, ans, builder, index+1);
+            builder.deleteCharAt(builder.length()-1);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(new ZiFuChuan().romanToInt("MCMXCIV"));
+        System.out.println(new ZiFuChuan().letterCombinations("23"));
     }
 
 }
