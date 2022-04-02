9259cc9c9db2413fe51518e26270ee230fe44f28
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-04-02 10:47:46 +0800

    22. 括号生成 45. 跳跃游戏 II

--- src/LeetcodeAgain/DynamicProgramming.java
+++ src/LeetcodeAgain/DynamicProgramming.java
@@ -1,5 +1,8 @@
 package LeetcodeAgain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DynamicProgramming {
 
     /**
@@ -29,10 +32,68 @@ public class DynamicProgramming {
 
     private String currLength(String s, int prev, int next) {
         int left = prev, right = next;
-        while (left >=0 && right<s.length() && s.charAt(left) == s.charAt(right)) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             --left;
             ++right;
         }
-        return s.substring(left+1, right);
+        return s.substring(left + 1, right);
+    }
+
+    /**
+     * 22. 括号生成
+     *
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        dfsForGenerate(ans, new StringBuilder(), n, n);
+        return ans;
+    }
+
+    private void dfsForGenerate(List<String> ans, StringBuilder builder, int left, int right) {
+        if (left < 0 || right < left) {
+            return;
+        }
+        if (left == 0 && right == 0) {
+            ans.add(builder.toString());
+        }
+        if (left > 0) {
+            builder.append("(");
+            dfsForGenerate(ans, builder, left - 1, right);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        if (right > left) {
+            builder.append(")");
+            dfsForGenerate(ans, builder, left, right - 1);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+
+    /**
+     * 45. 跳跃游戏 II
+     *
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int nextPosition = 0;
+        int maxPosition = 0;
+        int step = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == nextPosition) {
+                step++;
+                nextPosition = maxPosition;
+            }
+        }
+        return step;
+    }
+
+    public static void main(String[] args) {
+        DynamicProgramming programming = new DynamicProgramming();
+//        System.out.println(programming.jump(new int[]{2, 3, 0, 1, 4}));
+        System.out.println(programming.generateParenthesis(3));
     }
 }
