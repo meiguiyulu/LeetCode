70f7372ce8e33b36e099d9cb21409cdfb8f959bf
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-04-03 09:06:19 +0800

    10. 正则表达式匹配 53. 最大子数组和

--- src/LeetcodeAgain/DynamicProgramming.java
+++ src/LeetcodeAgain/DynamicProgramming.java
@@ -40,6 +40,44 @@ public class DynamicProgramming {
     }
 
     /**
+     * 10. 正则表达式匹配
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2]; // *可以代表0个
+                    if (matches(s, p, i, j - 1)) {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    private boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
+    /**
      * 22. 括号生成
      *
      * @param n
@@ -89,6 +127,24 @@ public class DynamicProgramming {
             }
         }
         return step;
+    }
+
+    /**
+     * 53. 最大子数组和
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int length = nums.length;
+        int ans = nums[0];
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        for (int i=1;i<length;i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i-1]);
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
