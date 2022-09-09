818888da2a7dc5d74cf27bcb9a853921a2a38fe2
玫瑰与鹿 <1609226090@qq.com>
2022-09-09 22:16:31 +0800

    1598. 文件夹操作日志搜集器

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -4551,6 +4551,26 @@ public class LeetCode {
         return sum;
     }
 
+    /**
+     * 1598. 文件夹操作日志搜集器
+     */
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        for (String log : logs) {
+            if ("./".equals(log)) {
+                continue;
+            } else if ("../".equals(log)) {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else {
+                depth++;
+            }
+        }
+        return depth;
+    }
+
+
     public static void main(String[] args) {
         LeetCode code = new LeetCode();
     }
