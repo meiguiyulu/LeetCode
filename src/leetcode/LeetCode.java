b13bd63ab5f1581faaecee5abfa1f10162b82635
玫瑰与鹿 <1609226090@qq.com>
2022-09-10 19:29:20 +0800

    669. 修剪二叉搜索树

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -4570,6 +4570,29 @@ public class LeetCode {
         return depth;
     }
 
+    /**
+     * 669. 修剪二叉搜索树
+     *
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        }
+    }
+
 
     public static void main(String[] args) {
         LeetCode code = new LeetCode();
