f7735e93e1098e942ac68022ee7aa216ba38275f
玫瑰与鹿 <1609226090@qq.com>
2022-09-12 10:32:21 +0800

    1608. 特殊数组的特征值

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -4593,6 +4593,28 @@ public class LeetCode {
         }
     }
 
+    /**
+     * 1608. 特殊数组的特征值
+     *
+     * @param nums
+     * @return
+     */
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (nums[i - 1] >= i && (i == n || nums[i] < i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
         LeetCode code = new LeetCode();
