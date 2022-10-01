0b2dd5ca9d3188449219d7d24a6fbc4b59ebd1cc
玫瑰与鹿 <1609226090@qq.com>
2022-10-01 21:34:07 +0800

    1694. 重新格式化电话号码

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -4668,8 +4668,43 @@ public class LeetCode {
         return res;
     }
 
+    /**
+     * 1694. 重新格式化电话号码
+     *
+     * @param number
+     * @return
+     */
+    public static String reformatNumber(String number) {
+        StringBuilder digits = new StringBuilder();
+        for (int i = 0; i < number.length(); ++i) {
+            char ch = number.charAt(i);
+            if (Character.isDigit(ch)) {
+                digits.append(ch);
+            }
+        }
+
+        int n = digits.length();
+        int pt = 0;
+        StringBuilder ans = new StringBuilder();
+        while (n > 0) {
+            if (n > 4) {
+                ans.append(digits.substring(pt, pt + 3) + "-");
+                pt += 3;
+                n -= 3;
+            } else {
+                if (n == 4) {
+                    ans.append(digits.substring(pt, pt + 2) + "-" + digits.substring(pt + 2, pt + 4));
+                } else {
+                    ans.append(digits.substring(pt, pt + n));
+                }
+                break;
+            }
+        }
+        return ans.toString();
+    }
+
 
     public static void main(String[] args) {
-        LeetCode code = new LeetCode();
+        System.out.println(reformatNumber("123 4-5678"));
     }
 }
