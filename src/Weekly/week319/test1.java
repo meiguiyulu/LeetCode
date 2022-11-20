eacb222e101efa8db30702205833436384a345e9
玫瑰与鹿 <1609226090@qq.com>
2022-11-20 15:46:53 +0800

    init week319

---
+++ src/Weekly/week319/test1.java
@@ -0,0 +1,18 @@
+package Weekly.week319;
+
+/**
+ * @Author LiuYunJie
+ * @Date 2022/11/20 15:44
+ **/
+public class test1 {
+
+    /**
+     *2469. 温度转换
+     * @param celsius
+     * @return
+     */
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.8 + 32.00};
+    }
+
+}
