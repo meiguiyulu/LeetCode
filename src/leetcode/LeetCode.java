9c044f105d3e8db747fc397d160f21bc6f07ad97
玫瑰与鹿 <1609226090@qq.com>
2023-02-12 09:24:40 +0800

    1138. 字母板上的路径

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -5269,6 +5269,46 @@ public class LeetCode {
         return (amount[0] + amount[1] + amount[2] + 1) / 2;
     }
 
+    /**
+     * 1138. 字母板上的路径
+     *
+     * @param target
+     * @return
+     */
+    public String alphabetBoardPath(String target) {
+        int cx = 0, cy = 0;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < target.length(); i++) {
+            char c = target.charAt(i);
+            int nx = (c - 'a') / 5;
+            int ny = (c - 'a') % 5;
+            if (nx < cx) {
+                for (int j = 0; j < cx - nx; j++) {
+                    res.append('U');
+                }
+            }
+            if (ny < cy) {
+                for (int j = 0; j < cy - ny; j++) {
+                    res.append('L');
+                }
+            }
+            if (nx > cx) {
+                for (int j = 0; j < nx - cx; j++) {
+                    res.append('D');
+                }
+            }
+            if (ny > cy) {
+                for (int j = 0; j < ny - cy; j++) {
+                    res.append('R');
+                }
+            }
+            res.append('!');
+            cx = nx;
+            cy = ny;
+        }
+        return res.toString();
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 5, 2, 4, 1};
         System.out.println(minOperations(nums));
