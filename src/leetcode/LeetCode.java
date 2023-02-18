1261b43e27ae8caff4a20ad1b06a3c143fd00440
玫瑰与鹿 <1609226090@qq.com>
2023-02-18 15:07:07 +0800

    1237. 找出给定方程的正整数解

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -5309,6 +5309,38 @@ public class LeetCode {
         return res.toString();
     }
 
+    /**
+     * 1237. 找出给定方程的正整数解
+     */
+    private final static int[] NOT_FOUND = new int[]{-1, -1};
+
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        long ans = 0;
+        Arrays.sort(nums);
+        for (int j = 0; j < nums.length; ++j) {
+            int r = lowerBound(nums, j, upper - nums[j] + 1);
+            int l = lowerBound(nums, j, lower - nums[j]);
+            ans += r - l;
+        }
+        return ans;
+    }
+
+    // 见 https://www.bilibili.com/video/BV1AP41137w7/
+    private int lowerBound(int[] nums, int right, int target) {
+        int left = -1; // 开区间 (left, right)
+        while (left + 1 < right) { // 区间不为空
+            // 循环不变量：
+            // nums[left] < target
+            // nums[right] >= target
+            int mid = (left + right) >>> 1;
+            if (nums[mid] < target)
+                left = mid; // 范围缩小到 (mid, right)
+            else
+                right = mid; // 范围缩小到 (left, mid)
+        }
+        return right;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 5, 2, 4, 1};
         System.out.println(minOperations(nums));
