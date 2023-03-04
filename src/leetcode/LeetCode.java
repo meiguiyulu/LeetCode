d6c1a824010c9718a36f78bceea44a81ba32edc8
玫瑰与鹿 <1609226090@qq.com>
2023-03-04 08:05:57 +0800

    982. 按位与为零的三元组

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -5341,6 +5341,30 @@ public class LeetCode {
         return right;
     }
 
+    /**
+     * 982. 按位与为零的三元组
+     *
+     * @param nums
+     * @return
+     */
+    public int countTriplets(int[] nums) {
+        int[] cnt = new int[1 << 16];
+        for (int x : nums) {
+            for (int y : nums) {
+                ++cnt[x & y];
+            }
+        }
+        int ans = 0;
+        for (int x : nums) {
+            for (int mask = 0; mask < (1 << 16); ++mask) {
+                if ((x & mask) == 0) {
+                    ans += cnt[mask];
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 5, 2, 4, 1};
         System.out.println(minOperations(nums));
