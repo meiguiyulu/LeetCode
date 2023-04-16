d6ee8fe4e7065e7c666c5b8a91ddaf748e0893cc
玫瑰与鹿 <1609226090@qq.com>
2023-04-16 10:07:04 +0800

    30. 串联所有单词的子串

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -5397,6 +5397,51 @@ public class LeetCode {
         return dp[(1 << n) - 1].stream().mapToInt(i -> i).toArray();
     }
 
+    /**
+     * 30. 串联所有单词的子串
+     * @param s
+     * @param words
+     * @return
+     */
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        int m = words.length, n = words[0].length(), ls = s.length();
+        for (int i = 0; i < n; i++) {
+            if (i + m * n > ls) {
+                break;
+            }
+            Map<String, Integer> differ = new HashMap<String, Integer>();
+            for (int j = 0; j < m; j++) {
+                String word = s.substring(i + j * n, i + (j + 1) * n);
+                differ.put(word, differ.getOrDefault(word, 0) + 1);
+            }
+            for (String word : words) {
+                differ.put(word, differ.getOrDefault(word, 0) - 1);
+                if (differ.get(word) == 0) {
+                    differ.remove(word);
+                }
+            }
+            for (int start = i; start < ls - m * n + 1; start += n) {
+                if (start != i) {
+                    String word = s.substring(start + (m - 1) * n, start + m * n);
+                    differ.put(word, differ.getOrDefault(word, 0) + 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                    word = s.substring(start - n, start);
+                    differ.put(word, differ.getOrDefault(word, 0) - 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                }
+                if (differ.isEmpty()) {
+                    res.add(start);
+                }
+            }
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 5, 2, 4, 1};
