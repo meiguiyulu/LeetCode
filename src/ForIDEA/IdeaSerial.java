eaa603114396390d8c6dfa4cd3f25717d0429424
玫瑰与鹿 <1609226090@qq.com>
2023-08-26 21:37:15 +0800

    228. 汇总区间

---
+++ src/ForIDEA/IdeaSerial.java
@@ -0,0 +1,36 @@
+package ForIDEA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author LiuYunJie
+ * @Date 2023/8/26 21:34
+ **/
+public class IdeaSerial {
+
+    /**
+     * 228. 汇总区间
+     */
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int low = i;
+            i++;
+            while (i < n && nums[i] == nums[i - 1] + 1) {
+                i++;
+            }
+            int high = i - 1;
+            StringBuilder temp = new StringBuilder(Integer.toString(nums[low]));
+            if (low < high) {
+                temp.append("->");
+                temp.append(nums[high]);
+            }
+            ret.add(temp.toString());
+        }
+        return ret;
+    }
+
+}
