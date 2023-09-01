4018e70fb8ef36a85472b07db4e04fd1079db7be
玫瑰与鹿 <1609226090@qq.com>
2023-09-01 20:59:29 +0800

    买钢笔和铅笔的方案数

--- src/ForIDEA/IdeaSerial.java
+++ src/ForIDEA/IdeaSerial.java
@@ -33,4 +33,23 @@ public class IdeaSerial {
         return ret;
     }
 
+    /**
+     * 买钢笔和铅笔的方案数
+     * @param total
+     * @param cost1
+     * @param cost2
+     * @return
+     */
+    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        if (cost1 < cost2) {
+            return waysToBuyPensPencils(total, cost2, cost1);
+        }
+        long res = 0, cnt = 0;
+        while (cnt * cost1 <= total) {
+            res += (total - cnt * cost1) / cost2 + 1;
+            cnt++;
+        }
+        return res;
+    }
+
 }
