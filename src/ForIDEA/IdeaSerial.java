f1fed2b39b4405fe2686d18f2cb07991d65c9cf3
玫瑰与鹿 <1609226090@qq.com>
2023-09-09 20:40:17 +0800

    207. 课程表

--- src/ForIDEA/IdeaSerial.java
+++ src/ForIDEA/IdeaSerial.java
@@ -1,7 +1,9 @@
 package ForIDEA;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @Author LiuYunJie
@@ -50,6 +52,45 @@ public class IdeaSerial {
             cnt++;
         }
         return res;
+    }
+
+    /**
+     * 207. 课程表
+     */
+    List<List<Integer>> edges;
+    int[] indeg;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        indeg = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+            ++indeg[info[0]];
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; ++i) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            ++visited;
+            int u = queue.poll();
+            for (int v: edges.get(u)) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return visited == numCourses;
     }
 
 }
