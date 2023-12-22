b3958f0dfbef5090249bd1f4b5319668a405e585
meiguiyulu <1609226090@qq.com>
2023-12-22 17:04:10 +0800

    1671. 得到山形数组的最少删除次数

--- src/ForIDEA/IdeaSerial.java
+++ src/ForIDEA/IdeaSerial.java
@@ -215,4 +215,49 @@ public class IdeaSerial {
         }
         return Arrays.stream(cardPoints).sum() - minSum;
     }
+
+    /**
+     * 1671. 得到山形数组的最少删除次数
+     * @param nums
+     * @return
+     */
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int[] pre = getLISArray(nums);
+        int[] reversed = reverse(nums);
+        int[] suf = getLISArray(reversed);
+        suf = reverse(suf);
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (pre[i] > 1 && suf[i] > 1) {
+                ans = Math.max(ans, pre[i] + suf[i] - 1);
+            }
+        }
+
+        return n - ans;
+    }
+
+    public int[] getLISArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
+    public int[] reverse(int[] nums) {
+        int n = nums.length;
+        int[] reversed = new int[n];
+        for (int i = 0; i < n; i++) {
+            reversed[i] = nums[n - 1 - i];
+        }
+        return reversed;
+    }
 }
