9d9c539dae8ccb1cd61c9145c39641ffa9d4418f
meiguiyulu <1609226090@qq.com>
2024-01-05 22:10:19 +0800

    1671. 得到山形数组的最少删除次数

--- src/ForIDEA/IdeaSerial.java
+++ src/ForIDEA/IdeaSerial.java
@@ -260,4 +260,26 @@ public class IdeaSerial {
         }
         return reversed;
     }
+
+
+    /**
+     * 1944. 队列中可以看到的人数
+     */
+    public int[] canSeePersonsCount(int[] heights) {
+        int n = heights.length;
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int[] res = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int h = heights[i];
+            while (!stack.isEmpty() && stack.peek() < h) {
+                stack.pop();
+                res[i]++;
+            }
+            if (!stack.isEmpty()) {
+                res[i]++;
+            }
+            stack.push(h);
+        }
+        return res;
 }
